4376c3bf32b65d020f5d0ce7a00db1510623af6d
dksgudwn <dksgudwn620@gmail.com>
2023-08-06 04:38:32 +0900

    돌 생성.ㅋ

---
+++ Assets/01.Scripts/Hyeongju/StoneSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneSpawner : MonoBehaviour
+{
+    public GameObject Stone;
+    float Timer = 0;
+    private void Update()
+    {
+        float spawnTime = Random.Range(2f, 3f);
+        Timer += Time.deltaTime;
+        if (Timer > spawnTime)
+        {
+            Instantiate(Stone);
+            Timer = 0;
+        }
+    }
+}
