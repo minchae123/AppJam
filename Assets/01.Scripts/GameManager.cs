7e2c68773397507bd314f54e376825f029a9a931
minchae123 <08minchae17@gmail.com>
2023-08-05 17:17:40 +0900

    ++

---
+++ Assets/01.Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+}
