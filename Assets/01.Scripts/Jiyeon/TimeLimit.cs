9635c14328ad3f97494df778edd0a26aee6c2242
jiyeon11 <leejiyeon415@gmail.com>
2023-08-05 20:12:02 +0900

    시간 안 멈춤

--- Assets/01.Scripts/Jiyeon/TimeLimit.cs
+++ Assets/01.Scripts/Jiyeon/TimeLimit.cs
@@ -10,28 +10,21 @@ public class TimeLimit : MonoBehaviour
     int min = 0;  //분
     int sec = 0;  //초
 
-    void Start()
-    {
-        
-    }
+    bool isStop = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (FlowerTouch())  //꽃에 도착
-        {
-        }
-        else  //도착 안하면 계속 시간 흐름
-        {
+        //도착 안하면 계속 시간 흐름
+        if(isStop != true)
             setTime -= Time.deltaTime;
-            min = (int)setTime / 60;
-            sec = (int)setTime % 60;
-
-            if(sec < 10)  //자릿수 맞추기
-                TimeText.text = "제한시간 " + min + ":0" + sec;
-            else
-                TimeText.text = "제한시간 " + min + ":" + sec;
-        }
+        min = (int)setTime / 60;
+        sec = (int)setTime % 60;
+     
+        if(sec < 10)  //자릿수 맞추기
+            TimeText.text = "제한시간 " + min + ":0" + sec;
+        else
+            TimeText.text = "제한시간 " + min + ":" + sec;
 
         if(setTime == 0)  //제한시간 종료
         {
@@ -39,9 +32,34 @@ public class TimeLimit : MonoBehaviour
         }
         
     }
-    bool FlowerTouch()  //꽃에 도착했을때 함수
-    {
 
-        return false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Flower"))
+        {
+            FlowerTouch();
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Flower"))
+        {
+            TimeReStart();
+        }
+    }
+
+
+    public void FlowerTouch()  //꽃에 도착했을때 함수
+    {
+        print("멈춤");
+        isStop = true;
+    }
+
+    public void TimeReStart()
+    {
+        print("시작");
+        isStop = false;
     }
 }
